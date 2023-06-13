12ccb37879ad9359d84a84a5da9f702de835eab0
Andres castañeda <estiven.casta12@gmail.com>
2023-06-13 12:37:17 -0500

    Se añade agregar, eliminar y completar tarea

--- taskManager.js
+++ taskManager.js
@@ -10,3 +10,29 @@ function addTask() {
   tasks.push({ id, description, status });
   console.log('Tarea añadida con exito.');
 }
+
+function deleteTask() {
+    const taskId = readline.questionInt('ID de la tarea a eliminar: ');
+  
+    const taskIndex = tasks.findIndex(task => task.id === taskId);
+    if (taskIndex !== -1) {
+      tasks.splice(taskIndex, 1);
+      console.log('Tarea eliminada con éxito.');
+    } else {
+      console.log('No se encontró una tarea con ese ID.');
+    }
+  }
+  
+  function completeTask() {
+    const taskId = readline.questionInt('ID de la tarea completada: ');
+  
+    const task = tasks.find(task => task.id === taskId);
+    if (task) {
+      task.status = 'completada';
+      console.log('Tarea marcada como completada.');
+    } else {
+      console.log('No se encontró una tarea con ese ID.');
+    }
+  }
+  
+  
