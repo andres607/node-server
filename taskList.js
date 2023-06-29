7142be744fe07e882d1c2d5fa560307ea2d68341
Andres castañeda <estiven.casta12@gmail.com>
2023-06-29 08:52:48 -0500

    se intenta el retorno de json

--- taskList.js
+++ taskList.js
@@ -1,4 +1,6 @@
-const readline = require('readline-sync');
+const express = require('express');
+const app = express();
+const port = 3000;
 
 let tasks = [];
 
@@ -6,105 +8,70 @@ function addTask() {
   return new Promise((resolve, reject) => {
     const indicator = readline.question('Ingrese el indicador de tarea:');
     const description = readline.question('Ingrese descripcion de la tarea:');
-  
 
-    tasks.push({
+    const task = {
+      id: tasks.length + 1,
       indicator,
       description,
       completed: false
-    });
+    };
 
-    resolve('La tarea se añadio correctamente.');
+    tasks.push(task);
+
+    resolve(task);
   });
 }
 
 function deleteTask() {
-    return new Promise((resolve, reject) =>{
-      const indicator = readline.question('Ingrese indicador de la tarea a eliminar:');
+  return new Promise((resolve, reject) => {
+    const indicator = readline.question('Ingrese indicador de la tarea a eliminar:');
 
-      const taskIndex = tasks.findIndex(task => task.indicator === indicator);
-        if (taskIndex !== -1) {
-          tasks.splice(taskIndex, 1);
-          resolve('Tarea eliminada con exito.');
-        } else {
-          reject('No se encontro la tarea con el indicador proporcionado.');
-        }
-    });
-  }
-
-  function completeTask() {
-    return new Promise((resolve, reject) => {
-      const indicator = readline.question('Ingrese el indicador de la tarea a completar: ');
-
-      const task = tasks.find(task => task.indicator === indicator);
-       if (task) {
-         task.completed = true;
-        resolve('Tarea completada.');
-     } else {
-       reject('No se encontro la tarea con el indicador proporcionado.');
+    const taskIndex = tasks.findIndex(task => task.indicator === indicator);
+    if (taskIndex !== -1) {
+      tasks.splice(taskIndex, 1);
+      resolve('Tarea eliminada con exito.');
+    } else {
+      reject('No se encontro la tarea con el indicador proporcionado.');
     }
   });
 }
-  
-  function showMenu() {
-    console.log('--- Lista de Tareas ---');
-    console.log('1. Añadir tarea');
-    console.log('2. Eliminar tarea');
-    console.log('3. Marcar tarea como completada');
-    console.log('4. Mostrar lista de tareas');
-    console.log('0. Salir');
-  }
- 
-  async function run() {
-    let choice = '';
-  
-    while (choice !== '0') {
-      showMenu();
-      choice = readline.question('Elija una opcion: ');
-  
-      switch (choice) {
-        case '1':
-          try {
-            const addMessage = await addTask();
-            console.log(addMessage);
-          } catch (error) {
-            console.log('Error:', error);
-          }
-          break;
-        case '2':
-          deleteTask()
-            .then(message => console.log(message))
-            .catch(error => console.log('Error:', error));
-          break;
-        case '3':
-          completeTask()
-            .then(message => console.log(message))
-            .catch(error => console.log('Error:', error));
-          break;
-        case '4':
-          try {
-            const taskList = await viewTasks();
-            console.log('=== Lista de Tareas ===');
-            taskList.forEach((task, index) => {
-              console.log(`Tarea ${index + 1}:`);
-              console.log(`Indicador: ${task.indicator}`);
-              console.log(`Descripcion: ${task.description}`);
-              console.log(`Estado: ${task.completed ? 'Completada' : 'Pendiente'}`);
-              console.log('-------------------------');
-            });
-          } catch (error) {
-            console.log('Error:', error);
-          }
-          break;
-        case '0':
-          console.log('Saliendo del programa.');
-          break;
-        default:
-          console.log('Opcion invalida. Intente nuevamente.');
-      }
-  
-      console.log('\n');
-    }
-  }
 
-  run();
+function completeTask() {
+  return new Promise((resolve, reject) => {
+    const indicator = readline.question('Ingrese el indicador de la tarea a completar: ');
+
+    const task = tasks.find(task => task.indicator === indicator);
+    if (task) {
+      task.completed = true;
+      resolve('Tarea completada.');
+    } else {
+      reject('No se encontro la tarea con el indicador proporcionado.');
+    }
+  });
+}
+
+function showMenu() {
+  console.log('--- Lista de Tareas ---');
+  console.log('1. Añadir tarea');
+  console.log('2. Eliminar tarea');
+  console.log('3. Marcar tarea como completada');
+  console.log('4. Mostrar lista de tareas');
+  console.log('0. Salir');
+}
+
+app.get('/tasks', (req, res) => {
+  res.json(tasks);
+});
+
+async function startServer() {
+  try {
+    await run();
+    app.listen(port, () => {
+      console.log(`Servidor escuchando en http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+startServer();
